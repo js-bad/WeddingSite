9ba110fc47ce0268256cbdea0bf796fa29dd649e
js-bad <kalebroyolson@gmail.com>
2022-11-13 21:07:05 -0600

    Vacc page font changes

--- wedding-site/src/RSVP/ConfirmVaccination.js
+++ wedding-site/src/RSVP/ConfirmVaccination.js
@@ -7,12 +7,12 @@ export default function ConfirmVaccination({ rootCtrl, show, name, updateVaccina
                 `confirm-vacc-content 
                 ${rootCtrl.isMobile ? 'confirm-vacc-content-mobile' : ''} 
                 straight-font`}>
-                <span>
+                <span style={{ fontSize: "0.9em"}}>
                     To ensure the safety of all wedding guests, the Bride and Groom respectfully request 
                     that all attendees are vaccinated for COVID-19.
                 </span><br/><br/>
-                <span>
-                    Please confirm that guest '{name}' has been vaccinated for COVID-19, or will be by 4/22/23:
+                <span style={{ fontSize: "1.3em", fontWeight: "bold"}}>
+                    Please confirm that '{name}' has been vaccinated for COVID-19, or will be by 4/22/23:
                 </span><br/>
                 <button className="vacc-btn vacc-btn-yes" onClick={() => updateVaccination(true)}>Yes</button>
                 <button className="vacc-btn" onClick={() => updateVaccination(false)}>No</button>
