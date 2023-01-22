5665b96f27cb73dda8afcdb4f84c25400fd8f4d8
js-bad <kalebroyolson+github@gmail.com>
2023-01-22 17:05:25 -0600

    Add RSVP notes

--- wedding-site/src/RSVP/RSVP.js
+++ wedding-site/src/RSVP/RSVP.js
@@ -12,7 +12,7 @@ export default function RSVP({ rootCtrl }) {
     }
 
     return (
-        <>
+        <div style={{position:"relative"}}>
             <div className='init-rsvp-section straight-font'>
                 <label className="init-rsvp-text straight-font" htmlFor='init-rsvp-name'>Enter name as it appears on invitation</label>
                 <div className='init-rsvp-input-section'>
@@ -20,13 +20,24 @@ export default function RSVP({ rootCtrl }) {
                     onChange={e => setName(e.target.value)} onKeyUp={e => handleKeyUp(e)} />
                     <button className='init-rsvp-enter curvy-font' onClick={e => ctrl.nameLookup(name)}>Submit</button>
                 </div>
-                <span className='init-rsvp-note'>(Please limit attendees to the guests specified on your invitation)</span>
+                { rootCtrl.isDesktop && <span className='init-rsvp-note'>(Please limit attendees to the guests specified below)</span>}
             </div>
-            
+            { rootCtrl.isDesktop?
+            <div id="rsvp-disclaimers" className='straight-font'>
+                <span style={{display: 'block'}}>*Event is 21+</span>
+                <span style={{display: 'block'}}>*COVID-19 Vaccination Required</span>
+            </div>
+            :
+            <div id="rsvp-disclaimers-mobile" className='straight-font'>
+                <span style={{display: 'block'}}>*Event is 21+</span>
+                <span style={{display: 'block'}}>*COVID-19 Vaccination Required</span>
+                <span style={{display: 'block'}}>*Please limit attendees to the guests specified below</span>
+            </div>
+            }
             
             <div className='guest-rsvps'>
                 {ctrl.guestRSVPs}
             </div>
-        </>
+        </div>
     )
 }
